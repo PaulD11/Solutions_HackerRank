120fb73a414e0cb5c34ae7b2d3c77be6a5833c1e
Paul Dederer <paul.dederer@icloud.com>
2019-04-18 20:45:44 +0200

    Designer PDF Viewer hackerrank solution

---
+++ Designer-pdf-viewer/designer-pdf-viewer.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+//################### SOLUTION #############################
+
+function designerPdfViewer(h, word) {
+    let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    let tallest = 0;
+    let index = 0;
+    let splittedWord = word.split('');
+
+
+    for (let i = 0; i < splittedWord.length; i++){
+        index = alph.indexOf(splittedWord[i]);
+        if (tallest < h[index]) {
+            tallest = h[index];
+        }
+    }
+    return tallest * splittedWord.length;
+}
+
+//################### SOLUTION #############################
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const h = readLine().split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word = readLine();
+
+    let result = designerPdfViewer(h, word);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
